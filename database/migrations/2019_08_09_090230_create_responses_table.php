375a532c43daa4b136df424c673b2051c16d6419
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-08-09 09:18:46 +0000

    make Model and migrations

---
+++ database/migrations/2019_08_09_090230_create_responses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('comment_id');
+            $table->integer('response_profile_id');
+            $table->text('response_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
