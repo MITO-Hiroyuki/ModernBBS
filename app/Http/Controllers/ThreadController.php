a069739d77d095fc9147bb1411f6cc86ed9ce69c
HiroyukiMito <diamonddust0225@gmail.com>
2019-08-15 07:12:18 +0000

    edited ThreadController add function index: view, show: see next, store: post thread

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -11,23 +11,46 @@ class ThreadController extends Controller
 		return view('thread.create');
 	}
 	
-	public function create(Request $request_thread)
+	public function index()
 	{
-		$this->validate($request_thread, Thread::$rules);
+		$threads = Thread::all();
+		return view::make('thread.index')->with('threads', $threads);
+	}
+	
+	public function show()
+	{
+		$thread = Thread::find($id);
+		return view::make('thread.detail')->with('thread, $thread');
+	}
+	
+	public function store()
+	{
+		$rules = [
+			'thread_title' => 'required',
+			'body' => 'required',
+			'category_id' => 'required',
+			];
 		
-		$thread = new Thread;
+		$messages = array(
+			'thread_title.required' => 'タイトルを正しく入力して下さい',
+			'body.required' => '本文を正しく入力して下さい',
+			'category_id.required' => 'カテゴリーを選択して下さい',
+			);
 		
-		$thread->thread_profile_id = $request_thread->profile()->id;
-		$thread->category_id = $request_thread->category()->id;
+		$validator = $Varidator::make(Input::all(), $rules, $messages);
 		
-		$thread_form = $request_thread->all();
-		
-		unset($form['_token']);
-		
-		$thread->fill($thread_form);
-		$thread->save();
-		
-		return redirect('thread');
+		if ($validator->passes()) {
+			$thread = new Thread;
+			$thread->thread_title = Input::get('thread_title');
+			$thread->body = Input::get('body');
+			$thread->category_id = Input::get('category_id');
+			$thread->save();
+			return Redirect::back()->with('message', '投稿が完了しました');
+		} else {
+			return Redirect::back()
+				->withErrors($validator)
+				->withInput();
+		}
 	}
 	
 	public function delete(Request $request_thread)
