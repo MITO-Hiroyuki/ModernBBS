4fd6c6a4f379827dcdbbb077f87490e7f59a6beb
HiroyukiMito <diamonddust0225@gmail.com>
2019-08-19 08:47:17 +0000

    create CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+		public function comment()
+	{
+		$comment = Comment::all();
+		return view::make('thread.comment')->with('comments', $comments);
+	}
+	
+	public function show()
+	{
+		$comment = Comment::find($id);
+		return view::make('thread.response')->with('comment, $comment');
+	}
+	
+	public function store(Request $request)
+	{
+		$rules = [
+			'comment_text' => 'required',
+			];
+		
+		$messages = array(
+			'comment_text.required' => '本文を正しく入力して下さい',
+			);
+		
+		$thread->user_id = $request->user()->id;
+		
+		$validator = $Varidator::make(Input::all(), $rules, $messages);
+		
+		if ($validator->passes()) {
+			$comment = new Comment;
+			$comment->comment_text = Input::get('comment_text');
+			$thread->thread_id = Input::get('thread_id');
+			$thread->save();
+			return Redirect::back()->with('message', '投稿が完了しました');
+		} else {
+			return Redirect::back()
+				->withErrors($validator)
+				->withInput();
+		}
+	}
 }
