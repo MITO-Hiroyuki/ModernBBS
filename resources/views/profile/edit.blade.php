a11fa6367b5d880d0a95d3ec6a91d3fdcd5efee4
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-08-23 07:26:32 +0000

    add birthday gender hobby form in profile/edit.blade

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -15,6 +15,44 @@
                             @endforeach
                         </ul>
                     @endif
+                    <div class="row">
+                        <label class="col-md-2">誕生日</label>
+                            <div class="col-md-10">
+                                <select name="birth_year">
+                                    <option value="">--</option>
+                                        @foreach(range(2000,2020) as $year)
+                                            <option>{{ $year }}</option>
+                                        @endforeach
+                                </select>
+                                <span>年</span>
+                                <select name="birth_month">
+                                    <option value="">--</option>
+                                        @foreach(range(1,12) as $month)
+                                        <option value="{{ str_pad($month,2,0,STR_PAD_LEFT) }}">{{ $month }}</option>
+                                        @endforeach
+                                </select>
+                                <span>月</span>
+                                <select name="birth_day">
+                                    <option value="">--</option>
+                                        @foreach(range(1,31) as $day)
+                                        <option value="{{ str_pad($day,2,0,STR_PAD_LEFT) }}">{{ $day }}</option>
+                                        @endforeach
+                                </select>
+                                <span>日</span>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ $profile->gender }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile->hobby }}">
+                        </div>
+                    </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="introduction">自己紹介</label>
                         <div class="col-md-10">
