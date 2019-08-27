d757f7f2961eac05a40d81d469c139e78b0e540a
HiroyukiMito <diamonddust0225@gmail.com>
2019-08-27 02:57:16 +0000

    edited pass

--- app/Thread.php
+++ app/Thread.php
@@ -12,16 +12,16 @@ class Thread extends Model
 	}
 	public function Comment()
 	{
-		return $this->hasMany('Comment', 'thread_id');
+		return $this->hasMany('App\Comment', 'thread_id');
 	}
 	
 	public function Category()
 	{
-		return $this->belongsTo('Category', 'category_id');
+		return $this->belongsTo('App\Category', 'category_id');
 	}
 	
 	public function Profile()
 	{
-		return $this->belongsTo('Profile', 'thread_profile_id');
+		return $this->belongsTo('App\Profile', 'thread_profile_id');
 	}
 }
