8633e18446bc67fea03ceb857493b382531d58eb
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-08-29 08:32:50 +0000

    UPDATE : login.blade design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <div class="card card-default">
-                <div class="card-heading">{{ __('messages.Login') }}</div>
-
-                <div class="card-body">
+        <div class="col-md-8 mx-auto">
+            <div class="card card-default bg-light py-5">
+                <div class="card-body offset-md-3">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">{{ __('messages.E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -27,7 +25,7 @@
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">{{ __('messages.Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control" name="password" required>
                                 @if ($errors->has('password'))
                                     <span class="help-block">
