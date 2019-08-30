1f96d8023eb406d0fb86decdddf54fcb870cb78b
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-08-30 08:41:16 +0000

    FIX : ProfileMode required

--- app/Profile.php
+++ app/Profile.php
@@ -10,6 +10,9 @@ class Profile extends Model
     protected $guarded = array('id');
     
     public static $rules = array(
+            'birth_year' => 'required',
+            'birth_month' => 'required',
+            'birth_day' => 'required',
             'hobby' => 'required',
             'gender' => 'required',
             'introduction' => 'required',
