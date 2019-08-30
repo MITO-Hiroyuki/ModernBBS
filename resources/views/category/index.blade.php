efc43f64164bf99046d883bb5925277c94c4a5a0
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-08-30 02:14:36 +0000

    UPDATE : categoy/index.blade

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -4,54 +4,48 @@
 @section('content')
     <div class="container">
         <div class="row">
-            
-            
+            <div class="card-panel col-md-10">
                 <!--Bootstrap4 offset(Bootstrap3"offset-md-1")-->
-                        <div class="card card1 col-md-3 offset-md-1">
-                            <h3 class="text-center">{{ $category1->name }}</h3>
-                        </div>
-                        <div class="card card2 col-md-3 offset-md-1">
-                            <h3 class="text-center">{{ $category2->name }}</h3>
-                        </div>
-                        <div class="card card3 col-md-3 offset-md-1">
-                            <h3 class="text-center">{{ $category3->name }}</h3>
-                        </div>
-                        
-                        <div class="card card4 col-md-3 offset-md-1">
-                            <h3 class="text-center">{{ $category4->name }}</h3>
-                        </div>
-                        <div class="card card5 col-md-3 offset-md-1">
-                            <h3 class="text-center">{{ $category5->name }}</h3>
-                        </div>
-                        <div class="card card6 col-md-3 offset-md-1">
-                            <h3 class="text-center">{{ $category6->name }}</h3>
-                        </div>
+                <div class="card card1">
+                    <h3 class="text-center">{{ $category1->name }}</h3>
+                </div>
+                <div class="card card2">
+                    <h3 class="text-center">{{ $category2->name }}</h3>
+                </div>
+                <div class="card card3">
+                    <h3 class="text-center">{{ $category3->name }}</h3>
+                </div>
+                <div class="card card4">
+                    <h3 class="text-center">{{ $category4->name }}</h3>
+                </div>
+                <div class="card card5">
+                    <h3 class="text-center">{{ $category5->name }}</h3>
+                </div>
+                <div class="card card6">
+                    <h3 class="text-center">{{ $category6->name }}</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <p>新着スレッド一覧</p>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th>タイトル</th>
+                      <th>カテゴリー</th>
+                  </thead>
+                  @foreach($threads as $thread)
+                  <tbody>
+                    <tr>
+                      <td><a href="{{ action('ThreadController@show',['id' => $thread->id] ) }}">{{ $thread->thread_title }}</td>
+                      <td>{{ $thread->Category->name }}</td>
+                    </tr>
+                  </tbody>
+                  @endforeach
+                </table>
+                {{ $threads->links() }}
+            </div>
+            </div>
         </div>
-    <div class="row mx-auto">
-        <h5>新着スレッド一覧</h5>
-      <div class="table-responsive">
-        <table class="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th>タイトル</th>
-              <th>カテゴリー</th>
-              <th>投稿者</th>
-              <th>投稿日</th>
-            </tr>
-          </thead>
-          @foreach($threads as $thread)
-          <tbody>
-            <tr>
-              <td><a href="{{ action('ThreadController@show',['id' => $thread->id] ) }}">{{ $thread->thread_title }}</td>
-              <td>{{ $thread->Category->name }}</td>
-              <td>{{ $thread->Profile->User->name }}</td>
-              <td>{{ $thread->created_at }}</td>
-            </tr>
-          </tbody>
-          @endforeach
-        </table>
-        {{ $threads->links() }}
-        </div>
-    </div>
     </div>
 @endsection
