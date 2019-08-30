5867cdb3b81d3f81a90ed691e3d90c1f1b77c69d
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-08-30 08:19:17 +0000

    UPDATE : profile view

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,43 +3,38 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <h2>{{ $profile->User->name }}プロフィール</h2>
-        </div>
-        <div class="row">
-            <label class="col-md-2">氏名</label>
-                <div class="col-md-8">
+            <h1>{{ $profile->User->name }}のプロフィール</h1>
+        <div class="jumbotron">
+            <div class="profile">
+                <div class="h3 display-4">
                     {{ $profile->User->name }}
                 </div>
         </div>
         <div class="row">
-            <label class="col-md-2">誕生日</label>
-                <div class="col-md-8">
+            <label class="h5 col-md-2">誕生日</label>
+                <div class="h3 col-md-8">
                     {{ $profile->birth_year }}年
                     {{ $profile->birth_month }}月
                     {{ $profile->birth_day }}日
                 </div>
         </div>
         <div class="row">
-            <label class="col-md-2">性別</label>
-                <div class="col-md-8">
+            <label class="h5 col-md-2">性別</label>
+                <div class="h3 col-md-8">
                     {{ $profile->gender }}
                 </div>
         </div>
         <div class="row">
-            <label class="col-md-2">趣味</label>
-                <div class="col-md-8">
+            <label class="h5 col-md-2">趣味</label>
+                <div class="h3 col-md-8">
                     {{ $profile->hobby }}
                 </div>
         </div>
         <div class="row">
-            <label class="col-md-2">自己紹介</label>
-                <div class="col-md-8">
+            <label class="h5 col-md-2">自己紹介</label>
+                <div class="h3 col-md-8">
                     {{ $profile->introduction }}
                 </div>
-        </div>
-        <div class="row">
-            <a href="{{ action('CategoryController@index') }}" role="button" class="btn btn-success">TOP画面</a>
         </div>
     </div>
 @endsection
