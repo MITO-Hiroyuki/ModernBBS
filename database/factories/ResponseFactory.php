8e888581a8f3f49e22f989188b853db4dbcd24ad
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-09-02 07:48:55 +0000

    FIX : follows_table

--- database/factories/ResponseFactory.php
+++ database/factories/ResponseFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Response::class, function (Faker $faker) {
     return [
             'comment_id' => $faker->numberBetween(1,20),
-            'response_profile_id' => $faker->numberBetween(1,2),
+            'user_id' => $faker->numberBetween(1,2),
             'response_text' => 'test_response',
     ];
 });
--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Thread::class, function (Faker $faker) {
     return [
             'category_id' => $faker->numberBetween(1,6),
-            'thread_profile_id' => $faker->numberBetween(1,2),
+            'user_id' => $faker->numberBetween(1,2),
             'thread_title' => 'test',
             'comment_count' => $faker->numberBetween(1,6),
             'body' => 'testtest',
