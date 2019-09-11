11f2fda99e3785327fdbb434ea8d0b489aa5faf2
HiroyukiMito <diamonddust0225@gmail.com>
2019-09-11 08:20:23 +0000

    ThreadList UI

--- resources/views/thread/index.blade.php
+++ resources/views/thread/index.blade.php
@@ -9,25 +9,32 @@
 		</div>
 		
 		@foreach($category_threads as $category_thread)
-			<h2>{{ $category_thread->thread_title }}</h2>
-			<p>投稿日：{{ date("Y年 m月 d日",strtotime($category_thread->created_at)) }}</p>
-			<p><a href="{{ action('ProfileController@get_profile', ['id' => $category_thread->profile_id]) }}">
-				投稿者：
-				@if ($category_thread->user != null)
-					{{ $category_thread->user->name }}
-				@endif
-			</a></p>
-			<p>{{ $category_thread->body }}</p>
+			<div class="card-header">
+				<h2>{{ $category_thread->thread_title }}</h2>
+			</div>
 			
-			@if($category_thread->follows->where('user_id',$user_id)->isNotEmpty())
-		  	<p>{{ link_to("/bbs/comment/comment/{$category_thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}</p>
-		  	@elseif($category_thread->user_id == $user_id)
-		  	<p>{{ link_to("/bbs/comment/comment/{$category_thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}</p>
-		  	@else
-		  	<p><a href="{{ action('FollowController@store', ['id' => $category_thread->user_id]) }}" role="button" class="btn btn-success">follow</a></p>
-		  	@endif
-		  	
-		  	
+			<div class="card-body">
+				<p>{{ $category_thread->body }}</p>
+				
+				@if($category_thread->follows->where('user_id',$user_id)->isNotEmpty())
+			  	<p>{{ link_to("/bbs/comment/comment/{$category_thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}</p>
+			  	@elseif($category_thread->user_id == $user_id)
+			  	<p>{{ link_to("/bbs/comment/comment/{$category_thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}</p>
+			  	@else
+			  	<p><a href="{{ action('FollowController@store', ['id' => $category_thread->user_id]) }}" role="button" class="btn btn-success">follow</a></p>
+			  	@endif
+			</div>
+			
+			<div class="card-hooter">
+				<p><a href="{{ action('ProfileController@get_profile', ['id' => $category_thread->profile_id]) }}">
+					投稿者：
+					@if ($category_thread->user != null)
+						{{ $category_thread->user->name }}
+					@endif
+				</a></p>
+				<p>投稿日：{{ date("Y年 m月 d日",strtotime($category_thread->created_at)) }}</p>
+			</div>
+			
 		@endforeach
 	</div>
 @endsection
