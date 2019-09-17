1839cec9af4fdfc49c083ff293fb27e5f0da38be
HiroyukiMito <diamonddust0225@gmail.com>
2019-09-17 05:24:32 +0000

    changed design [mythreds]

--- resources/views/profile/mythreads.blade.php
+++ resources/views/profile/mythreads.blade.php
@@ -4,25 +4,46 @@
 	<div class="col-md-8 mx-auto">
 		
 		<div class="">
-			<a href="{{ action('ThreadController@create') }}"><button type="button">新規情報投稿</button></a>
-			<hr />
+			<a href="{{ action('ThreadController@create') }}">
+				<button type="button" class="btn btn-primary btn-lg btn-block">新規スレッド投稿</button>
+			</a>
 		</div>
 		
 		@foreach($follows as $follow)
-		 @foreach($follow->threads as $thread)
-			<h2>{{ $thread->thread_title }}</h2>
-			<p>投稿日：{{ date("Y年 m月 d日",strtotime($thread->created_at)) }}</p>
-			<p><a href="{{ action('ProfileController@get_profile', ['id' => $thread->profile_id]) }}">
-				投稿者：
-				@if ($thread->user != null)
-					{{ $thread->user->name }}
-				@endif
-			</a></p>
-			<p>{{ $thread->body }}</p>
-
-		  	<p>{{ link_to("/bbs/comment/comment/{$thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}</p>
-		  	
-		 @endforeach
+		@foreach($follow->threads as $thread)
+		
+			<div class="card mt-4">
+				
+				<div class="card-header">
+					<h2>{{ $thread->thread_title }}</h2>
+				</div>
+			
+				<div class="card-body">
+					<p>{{ $thread->body }}</p>
+					@if($thread->follows->where('user_id',$user_id)->isNotEmpty())
+					{{ link_to("/bbs/comment/comment/{$thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}
+					@elseif($thread->user_id == $user_id)
+					{{ link_to("/bbs/comment/comment/{$thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}
+					@else
+					{{ link_to("/bbs/comment/comment/{$thread->id}", 'コメントを読む', array('class' => 'btn btn-primary')) }}
+					<a href="{{ action('FollowController@store', ['id' => $thread->user_id]) }}" role="button" class="btn btn-success">follow</a>
+					@endif
+				</div>
+				
+				<div class="card-footer">
+					<a href="{{ action('ProfileController@get_profile', ['id' => $thread->profile_id]) }}">
+						投稿者：
+						@if ($thread->user != null)
+							{{ $thread->user->name }}
+						@endif
+					</a>
+					&nbsp;
+					投稿日：{{ date("Y年 m月 d日",strtotime($thread->created_at)) }}
+				</div>
+				
+			</div>
 		@endforeach
+		@endforeach
+		
 	</div>
 @endsection
